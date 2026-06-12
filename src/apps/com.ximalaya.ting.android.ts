7d3fa10f17db1041439bea7108caf5b90294454d
DengShilin <dengsl.dev@gmail.com>
2026-06-12 13:48:43 +0800

    添加全局配置

--- src/apps/com.ximalaya.ting.android.ts
+++ src/apps/com.ximalaya.ting.android.ts
@@ -16,5 +16,33 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '7天内不再提醒广告',
+      rules: [{ key: 1, fastQuery: true, matches: '[text="7天内不再提醒"]' }],
+    },
+    {
+      key: 3,
+      name: '推荐评分弹窗',
+      rules: [
+        {
+          key: 1,
+          fastQuery: true,
+          matches:
+            '[text="您有多大意愿向他人推荐使用喜马拉雅？"] + [vid="host_iv_close"]',
+        },
+      ],
+    },
+    {
+      key: 4,
+      name: '添加到桌面',
+      rules: [
+        {
+          key: 1,
+          fastQuery: true,
+          matches: '[text="添加历史到桌面"] + [vid="listen_close_iv"]',
+        },
+      ],
+    },
   ],
 });
